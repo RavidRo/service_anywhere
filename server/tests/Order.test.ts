016441663387e137a98af6a3ffc07bebae287fce
domberman <barakom@post.bgu.ac.il>
2022-01-25 17:20:31 +0200

    added tests

---
+++ server/tests/Order.test.ts
@@ -0,0 +1,34 @@
+import { Order } from "../Logic/Order";
+import { Location } from "../api";
+
+const order = require('../Logic/Order');
+
+var firstOrder: string;
+
+beforeAll(() => {
+    firstOrder = order.Order.createOrder(['a','b'])    
+});
+
+test('createOrder should return an order ID', () => {
+    expect(firstOrder).toBeTruthy()
+    expect(typeof firstOrder).toBe('string')
+});
+
+test('createOrder should create unique order Ids', () => {
+    expect(order.Order.createOrder(['a','b'])).not.toBe(firstOrder)
+});
+
+test('giveFeedback should return true', () => {
+    expect(order.Order.delegate(firstOrder, (o: Order) => {return o.giveFeedback('good', 5)})).toBe(true)
+});
+
+test('updateLocationGuest and getGuestLocation should have corresponding locations', () => {
+    var location: Location = {x: 1, y: 1}
+    expect(order.Order.delegate(firstOrder, (o: Order) => {return o.updateLocationGuest(location)})).toBe(true)
+    expect(order.Order.getGuestLocation(firstOrder)).toEqual(location)
+});
+
+test('order arrival test', () => {
+    order.Order.delegate(firstOrder, (o: Order) => {o.orderArrived()})
+    expect(order.Order.delegate(firstOrder, (o: Order) => {return o.hasOrderArrived()})).toBe(true)
+});
---
+++ server/tests/WaiterOrder.test.ts
@@ -0,0 +1,48 @@
+import { Order } from "../Logic/Order";
+import { WaiterOrder } from "../Logic/WaiterOrder";
+
+let waiter: string;
+let order: string;
+
+beforeAll(() => {
+    waiter = WaiterOrder.connectWaiter()
+    order = Order.createOrder(['a','b']);
+});
+
+test('connect waiter should return a waiter ID', () => {
+    expect(waiter).toBeTruthy()
+    expect(typeof waiter).toBe('string')
+});
+
+test('connect waiter should create unique waiter Ids', () => {
+    expect(WaiterOrder.connectWaiter()).not.toBe(waiter)
+});
+
+test('get waiter by order with our order should return nothing', () => {
+    const waiters: string[] = WaiterOrder.getWaiterByOrder(order)
+    expect(waiters.length).toBe(0)
+});
+
+test('get waiter order with our waiter should return nothing', () => {
+    const orders: string[] = WaiterOrder.getWaiterOrder(waiter)
+    expect(orders.length).toBe(0)
+});
+
+describe('assign a waiter to an order', () => {
+
+    beforeAll(() => {
+        WaiterOrder.assignWaiter(order, waiter);
+    });
+    
+    test('get waiter by order with our order should return our waiter', () => {
+        const waiters: string[] = WaiterOrder.getWaiterByOrder(order)
+        expect(waiters.length).toBe(1)
+        expect(waiters[0]).toEqual(waiter)
+    });
+
+    test('get waiter order with our waiter should return our order', () => {
+        const orders: string[] = WaiterOrder.getWaiterOrder(waiter)
+        expect(orders.length).toBe(1)
+        expect(orders[0]).toEqual(order)
+    });
+});
