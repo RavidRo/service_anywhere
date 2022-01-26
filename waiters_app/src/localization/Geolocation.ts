03f553584bd09909198946e5ac68bee7a09c9df5
Ravid Rom <romravid@gmail.com>
2022-01-26 13:48:03 +0200

    Created Localization

---
+++ waiters_app/src/localization/Geolocation.ts
@@ -0,0 +1,44 @@
+import Location from '../data/Location';
+import GeolocationAdapter from './GeolocationAdapter';
+
+import {ILocationService} from './ILocationService';
+import LocationMap from './Map';
+
+export default class Geolocation implements ILocationService {
+	private geolocationAdapter: GeolocationAdapter;
+	private map: LocationMap;
+	constructor(corners: Corners) {
+		this.map = new LocationMap(corners);
+	}
+
+	private translateFunction(successCallback: (location: Location) => void) {
+		return (location: GPS) => {
+			const newLocation = this.map.translateGps(location);
+			successCallback(newLocation);
+		};
+	}
+
+	watchLocation(
+		successCallback: (location: Location) => void,
+		errorCallback: (error: string) => void
+	) {
+		this.geolocationAdapter.watchLocation(
+			this.translateFunction(successCallback),
+			errorCallback
+		);
+	}
+
+	getLocation(
+		successCallback: (location: Location) => void,
+		errorCallback: (error: string) => void
+	): void {
+		this.geolocationAdapter.getLocation(
+			this.translateFunction(successCallback),
+			errorCallback
+		);
+	}
+
+	stopWatching(): void {
+		this.geolocationAdapter.stopWatching();
+	}
+}
