4c2542bf576b43ca751fbba3afe3d0582c68ef77
domberman <domberman@users.noreply.github.com>
2022-02-08 14:15:13 +0000

    Apply formatting changes

--- server/tests/WaiterOrder.test.ts
+++ server/tests/WaiterOrder.test.ts
@@ -1,48 +1,47 @@
-import { Order } from "../Logic/Order";
-import { WaiterOrder } from "../Logic/WaiterOrder";
+import {Order} from '../Logic/Order';
+import {WaiterOrder} from '../Logic/WaiterOrder';
 
 let waiter: string;
 let order: string;
 
 beforeAll(() => {
-    waiter = WaiterOrder.connectWaiter()
-    order = Order.createOrder(['a','b']);
+	waiter = WaiterOrder.connectWaiter();
+	order = Order.createOrder(['a', 'b']);
 });
 
 test('connect waiter should return a waiter ID', () => {
-    expect(waiter).toBeTruthy()
-    expect(typeof waiter).toBe('string')
+	expect(waiter).toBeTruthy();
+	expect(typeof waiter).toBe('string');
 });
 
 test('connect waiter should create unique waiter Ids', () => {
-    expect(WaiterOrder.connectWaiter()).not.toBe(waiter)
+	expect(WaiterOrder.connectWaiter()).not.toBe(waiter);
 });
 
 test('get waiter by order with our order should return nothing', () => {
-    const waiters: string[] = WaiterOrder.getWaiterByOrder(order)
-    expect(waiters.length).toBe(0)
+	const waiters: string[] = WaiterOrder.getWaiterByOrder(order);
+	expect(waiters.length).toBe(0);
 });
 
 test('get waiter order with our waiter should return nothing', () => {
-    const orders: string[] = WaiterOrder.getWaiterOrder(waiter)
-    expect(orders.length).toBe(0)
+	const orders: string[] = WaiterOrder.getWaiterOrder(waiter);
+	expect(orders.length).toBe(0);
 });
 
 describe('assign a waiter to an order', () => {
+	beforeAll(() => {
+		WaiterOrder.assignWaiter(order, waiter);
+	});
 
-    beforeAll(() => {
-        WaiterOrder.assignWaiter(order, waiter);
-    });
-    
-    test('get waiter by order with our order should return our waiter', () => {
-        const waiters: string[] = WaiterOrder.getWaiterByOrder(order)
-        expect(waiters.length).toBe(1)
-        expect(waiters[0]).toEqual(waiter)
-    });
+	test('get waiter by order with our order should return our waiter', () => {
+		const waiters: string[] = WaiterOrder.getWaiterByOrder(order);
+		expect(waiters.length).toBe(1);
+		expect(waiters[0]).toEqual(waiter);
+	});
 
-    test('get waiter order with our waiter should return our order', () => {
-        const orders: string[] = WaiterOrder.getWaiterOrder(waiter)
-        expect(orders.length).toBe(1)
-        expect(orders[0]).toEqual(order)
-    });
+	test('get waiter order with our waiter should return our order', () => {
+		const orders: string[] = WaiterOrder.getWaiterOrder(waiter);
+		expect(orders.length).toBe(1);
+		expect(orders[0]).toEqual(order);
+	});
 });
