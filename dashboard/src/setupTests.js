48e14221d96a82b1776a331f69d77de577ed61be
TommerFed <tommerico2000@gmail.com>
2022-02-23 11:44:17 +0200

    lintr

--- dashboard/src/setupTests.js
+++ dashboard/src/setupTests.js
@@ -4,17 +4,3 @@
 // learn more: https://github.com/testing-library/jest-dom
 // require('@testing-library/jest-dom');
 // require('../src/model/ordersModel');
-
-// const orderModel = new ordersModel();
-
-// beforeEach(() => {
-// 	orderModel.orders = [];
-// });
-
-// // describe('test', () => {
-// // 	it('checking setting of orders', () => {
-// // 		const orders = ['a', 'b'];
-// // 		orderModel.orders = orders;
-// // 		expect(orderModel.orders).toEqual(orders);
-// // 	});
-// // });
