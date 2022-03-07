c211061de60efd8f8a033d93b03e08494ebc62a1
aviv7 <62387895+aviv7@users.noreply.github.com>
2022-03-07 12:08:07 +0200

    fixing automated checks

--- guests_app/src/ViewController/MainPageViewController.tsx
+++ guests_app/src/ViewController/MainPageViewController.tsx
@@ -1,19 +1,17 @@
-import Requests from 'guests_app/src/Networking/requests';
 import React, {useContext, useState} from 'react';
 import {Alert, PermissionsAndroid, Platform} from 'react-native';
 import {itemsContext, MyLocationContext, OrdersContext} from '../contexts';
 import {OrderID} from '../types';
 import {MainPage} from '../View/MainPageView';
-import ItemViewModel from '../ViewModel/ItemViewModel';
-import OrderViewModel from '../ViewModel/OrderViewModel';
+
 //import {observer} from 'mobx-react-lite';
 
 export const MainPageViewController = () => {
 	const orderViewModel = useContext(OrdersContext);
-	const itemViewModel = useContext(itemsContext);
+//	const itemViewModel = useContext(itemsContext);
 	const locationViewModel = useContext(MyLocationContext);
 
-
+	// will be changed later to items from itemViewModel 
 	let items = new Map<string, number>([
 		['Item1ID', 1],
 		['Item2ID', 1],
