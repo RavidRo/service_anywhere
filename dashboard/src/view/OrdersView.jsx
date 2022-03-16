0440b488a0b57a62819be97ac6206f234f76114c
TommerFed <tommerico2000@gmail.com>
2022-03-16 14:09:33 +0200

    visual update

---
+++ dashboard/src/view/OrdersView.jsx
@@ -0,0 +1,68 @@
+import {makeStyles} from '@material-ui/core/styles';
+import {toJS} from 'mobx';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {observer} from 'mobx-react';
+import Box from '@mui/material/Box';
+import {DataGrid, GridActionsCellItem} from '@mui/x-data-grid';
+
+const _useStyles = makeStyles(theme => ({
+	root: {
+		width: 'fit-content',
+		color: theme.palette.text.secondary,
+		'& svg': {
+			margin: theme.spacing(2),
+		},
+		'& hr': {
+			margin: theme.spacing(0, 0.5),
+		},
+	},
+}));
+
+function OrdersView(props) {
+	const {
+		orders,
+		columns,
+		apiRef,
+		handleRowEditStart,
+		handleRowEditStop,
+		handleCellFocusOut,
+	} = props;
+
+	return (
+		<Box
+			sx={{
+				height: 500,
+				width: '100%',
+				'& .actions': {
+					color: 'text.secondary',
+				},
+				'& .textPrimary': {
+					color: 'text.primary',
+				},
+			}}>
+			<DataGrid
+				rows={toJS(orders)}
+				columns={columns}
+				apiRef={apiRef}
+				editMode='row'
+				onRowEditStart={handleRowEditStart}
+				onRowEditStop={handleRowEditStop}
+				onCellFocusOut={handleCellFocusOut}
+				componentsProps={{
+					toolbar: {apiRef},
+				}}
+			/>
+		</Box>
+	);
+}
+
+export default observer(OrdersView);
+OrdersView.propTypes = {
+	orders: PropTypes.array,
+	columns: PropTypes.array,
+	apiRef: PropTypes.object,
+	handleRowEditStart: PropTypes.func,
+	handleRowEditStop: PropTypes.func,
+	handleCellFocusOut: PropTypes.func,
+};
--- dashboard/src/viewController/WaiterDialogViewController.jsx
+++ dashboard/src/viewController/WaiterDialogViewController.jsx
@@ -3,19 +3,41 @@ import WaiterDialogView from '../view/WaiterDialogView';
 import propTypes from 'prop-types';
 
 export default function WaiterDialogViewController(props) {
-	const {onClose, open, waitersViewModel} = props;
+	const {waitersViewModel, orderId} = props;
+	const [open, setOpen] = React.useState(false);
+	const [assignedWaiter, setAssignedWaiter] = React.useState([]);
 
-	const handleClose = () => {
-		onClose('');
+	React.useEffect(() => {
+		let mounted = true;
+		waitersViewModel.getWaitersByOrder(orderId).then(assignedWaiter => {
+			if (mounted) {
+				setAssignedWaiter(assignedWaiter);
+			}
+		});
+		return () => (mounted = false);
+	}, []);
+
+	const handleOpen = () => {
+		setOpen(true);
 	};
 
-	const handleListItemClick = value => {
-		onClose(value);
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleListItemClick = waiter => {
+		if (waiter !== '') {
+			if (waitersViewModel.assignWaiter(orderId, waiter)) {
+				setAssignedWaiter(waiter);
+			}
+		}
 	};
 
 	return (
 		<WaiterDialogView
+			assignedWaiter={assignedWaiter}
 			waiters={waitersViewModel.waiters}
+			handleOpen={handleOpen}
 			handleClose={handleClose}
 			handleListItemClick={handleListItemClick}
 			open={open}
@@ -24,6 +46,7 @@ export default function WaiterDialogViewController(props) {
 }
 
 WaiterDialogViewController.propTypes = {
+	orderId: propTypes.number,
 	onClose: propTypes.func,
 	open: propTypes.bool,
 	waitersViewModel: propTypes.object,
