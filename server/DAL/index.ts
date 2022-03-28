28f71b96212b4aefc1ebc6fe2bb09c9b41d283d9
ravidro <ravidro@post.bgu.ac.il>
2022-03-28 19:24:08 +0300

    Removed src folder

--- server/DAL/index.ts
+++ server/DAL/index.ts
@@ -1,26 +1,4 @@
-// import 'reflect-metadata';
-// import DBManager from './DAL/dbManager';
-// import {User} from './entities/User';
-
-// let user: User = new User();
-// user.firstName = 'aviv';
-// user.lastName = 'almog';
-// user.age = 21;
-
-// let user2: User = new User();
-// user.firstName = 'aviv2';
-// user.lastName = 'almog';
-// user.age = 21;
-
-// let dbManager: DBManager = new DBManager();
-// dbManager.AddNewUser(user);
-// dbManager.AddNewUser(user2);
-
-// console.log("loading users from db")
-// dbManager.LoadUsers().then(users => {
-//     console.log(users.length);
-//     console.log(users);
-// })
+//* This is an example file
 
 import {AppDataSource} from './data-source';
 import {Guest} from './entities/Domain/Guest';
