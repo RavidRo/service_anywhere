52685a9b902f25c0ce9e8ca859f19cb201ba6466
domberman <48616609+domberman@users.noreply.github.com>
2022-06-02 15:53:44 +0300

    Update server/Logic/Order.ts
    Co-authored-by: Ravid Rom <romravid@gmail.com>

--- server/Logic/Order.ts
+++ server/Logic/Order.ts
@@ -95,7 +95,7 @@ export class Order implements IOrder {
 		return makeGood();
 	}
 
-	async assign(_waiterId: string[]): Promise<ResponseMsg<void>> {
+	async assign(_waiterIds: string[]): Promise<ResponseMsg<void>> {
 		return this.changeOrderStatus('assigned', true, true);
 	}
 
