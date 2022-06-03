0a370da62af1f2d4a0c46b1808d7503209881a04
TommerFed <tommerico2000@gmail.com>
2022-06-03 16:32:34 +0300

    dashboard fix

--- dashboard/__tests__/connectViewModelTests.ts
+++ dashboard/__tests__/connectViewModelTests.ts
@@ -41,19 +41,16 @@ const mockListOfWaiters: WaiterIDO[] = [
 	{
 		id: '1',
 		name: 'waiter 1',
-		available: false,
 	},
 	{
 		id: '2',
 		name: 'waiter 2',
-		available: true,
 	},
 ];
 const mockGetOrders = jest.fn(() => mockMakePromise(mockListOfOrders));
 const mockGetWaiters = jest.fn(() => mockMakePromise(mockListOfWaiters));
 const mockAssignWaiter = jest.fn((_orderId: string, waiterId: string) => {
-	return mockListOfWaiters.filter(waiter => waiter.id === waiterId)[0]
-		.available;
+	return true;
 });
 const mockGetWaitersByOrder = jest.fn((_orderId: string) =>
 	Promise.resolve(mockListOfOrders[0])
